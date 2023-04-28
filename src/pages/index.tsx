2aa06b2e7e70020b426fde38ad932414c4ddd31b
Patrick Ramser <pramser@gmail.com>
2023-04-28 12:57:51 -0400

    Convert everything over to generate interview questions for a job description passed in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,18 +13,20 @@ import ResizablePanel from "../components/ResizablePanel"
 
 const Home: NextPage = () => {
   const [loading, setLoading] = useState(false)
-  const [bio, setBio] = useState("")
+  const [jobDescription, setJobDescription] = useState("")
   const [vibe, setVibe] = useState<VibeType>("Professional")
   const [generatedBios, setGeneratedBios] = useState<String>("")
 
   const prompt =
     vibe === "Funny"
-      ? `Generate 2 funny twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure there is a joke in there and it's a little ridiculous. Make sure each generated bio is at max 20 words and base it on this context: ${bio}${
-          bio.slice(-1) === "." ? "" : "."
+      ? `Generate 3 funny interview questions clearly labeled "1." and "2." amd "3.". Make sure there is a joke in there and it's a little ridiculous. Make sure each generated bio is at max 30 words and base it on this context: ${jobDescription}${
+          jobDescription.slice(-1) === "." ? "" : "."
         }`
-      : `Generate 2 ${vibe} twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure each generated bio is at least 14 words and at max 20 words and base them on this context: ${bio}${
-          bio.slice(-1) === "." ? "" : "."
+      : `Generate 3 ${vibe} interview questions clearly labeled "1." and "2." and "3.". Make sure each generated question is at least 14 words and at max 30 words and base them on this context: ${jobDescription}${
+          jobDescription.slice(-1) === "." ? "" : "."
         }`
+
+  const LIST_NUM_REG_EX = /[1-9]. /
 
   const generateBio = async (e: any) => {
     e.preventDefault()
@@ -53,7 +55,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Twitter Generator</title>
+        <title>Opportun.me</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -61,7 +63,7 @@ const Home: NextPage = () => {
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/Nutlope/twitterbio"
+          href="https://github.com/pramser/opportunme"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -69,9 +71,8 @@ const Home: NextPage = () => {
           <p>Star on GitHub</p>
         </a>
         <h1 className="sm:text-6xl text-4xl max-w-2xl font-bold text-slate-900">
-          Generate your next Twitter bio in seconds
+          Generate interview questions based on a job description
         </h1>
-        <p className="text-slate-500 mt-5">18,167 bios generated so far.</p>
         <div className="max-w-xl">
           <div className="flex mt-10 items-center space-x-3">
             <Image
@@ -82,16 +83,16 @@ const Home: NextPage = () => {
               className="mb-5 sm:mb-0"
             />
             <p className="text-left font-medium">
-              Copy your current bio{" "}
+              Copy a job description{" "}
               <span className="text-slate-500">
-                (or write a few sentences about yourself)
+                (or try and summarize the role)
               </span>
               .
             </p>
           </div>
           <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
+            value={jobDescription}
+            onChange={(e) => setJobDescription(e.target.value)}
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
@@ -100,7 +101,7 @@ const Home: NextPage = () => {
           />
           <div className="flex mb-5 items-center space-x-3">
             <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
+            <p className="text-left font-medium">Select interview vibe.</p>
           </div>
           <div className="block">
             <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
@@ -111,7 +112,7 @@ const Home: NextPage = () => {
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
               onClick={(e) => generateBio(e)}
             >
-              Generate your bio &rarr;
+              Generate your questions &rarr;
             </button>
           )}
           {loading && (
@@ -136,13 +137,13 @@ const Home: NextPage = () => {
                 <>
                   <div>
                     <h2 className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto">
-                      Your generated bios
+                      Your generated questions
                     </h2>
                   </div>
                   <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                     {generatedBios
                       .substring(generatedBios.indexOf("1") + 3)
-                      .split("2.")
+                      .split(LIST_NUM_REG_EX)
                       .map((generatedBio) => {
                         return (
                           <div
