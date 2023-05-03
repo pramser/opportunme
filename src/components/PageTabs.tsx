53edbde2f80cf1731b95f686f9f82b7ed1615bab
Patrick Ramser <pramser@gmail.com>
2023-05-03 17:38:15 -0400

    Add tab for another potential tool

---
+++ src/components/PageTabs.tsx
@@ -0,0 +1,69 @@
+import { useRouter } from "next/router"
+
+type Tab = {
+  name: string
+  href: string
+}
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ")
+}
+
+export default function PageTabs({ tabs }: { tabs: Tab[] }) {
+  const router = useRouter()
+
+  return (
+    <div className="w-1/2">
+      <div className="sm:hidden">
+        <label htmlFor="tabs" className="sr-only">
+          Select a tab
+        </label>
+        <select
+          id="tabs"
+          name="tabs"
+          className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+          defaultValue={
+            tabs.find((tab: Tab) => router.pathname === tab.href)?.name
+          }
+        >
+          {tabs.map((tab) => (
+            <option key={tab.name}>{tab.name}</option>
+          ))}
+        </select>
+      </div>
+      <div className="hidden sm:block">
+        <nav
+          className="isolate flex divide-x divide-gray-200 rounded-lg shadow"
+          aria-label="Tabs"
+        >
+          {tabs.map((tab, tabIdx) => (
+            <a
+              key={tab.name}
+              href={tab.href}
+              className={classNames(
+                router.pathname === tab.href
+                  ? "text-gray-900"
+                  : "text-gray-500 hover:text-gray-700",
+                tabIdx === 0 ? "rounded-l-lg" : "",
+                tabIdx === tabs.length - 1 ? "rounded-r-lg" : "",
+                "group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10"
+              )}
+              aria-current={router.pathname === tab.href ? "page" : undefined}
+            >
+              <span>{tab.name}</span>
+              <span
+                aria-hidden="true"
+                className={classNames(
+                  router.pathname === tab.href
+                    ? "bg-indigo-500"
+                    : "bg-transparent",
+                  "absolute inset-x-0 bottom-0 h-0.5"
+                )}
+              />
+            </a>
+          ))}
+        </nav>
+      </div>
+    </div>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
+import "../styles/globals.css"
+
+// next
 import { Analytics } from "@vercel/analytics/react"
 import type { AppProps } from "next/app"
-import "../styles/globals.css"
+
+// components
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import PageTabs from "@/components/PageTabs"
+
+const tabs = [
+  { name: "Interview Questions", href: "/" },
+  { name: "Coming soon...", href: "#" },
+]
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 py-8 sm:mt-4 bg-white border-gray-600 lg:border drop-shadow-2xl">
+        <PageTabs tabs={tabs} />
         <Component {...pageProps} />
         <Analytics />
       </main>
