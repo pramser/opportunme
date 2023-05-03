2b4b7dd6d888a55d0ca67f035972e383ca7be088
Patrick Ramser <pramser@gmail.com>
2023-05-03 12:26:23 -0400

    Add meta targets for card preview when linking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,30 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Get help with your next, big opportunity."
+          />
+          <meta property="og:site_name" content="opportun.me" />
+          <meta
+            property="og:description"
+            content="Get help with your next, big opportunity."
+          />
+          <meta property="og:title" content="Opportun.me" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Opportun.me" />
+          <meta
+            name="twitter:description"
+            content="Get help with your next, big opportunity."
+          />
+          <meta
+            property="og:image"
+            content="https://opportun.me/og-image.png"
+          />
+          <meta
+            name="twitter:image"
+            content="https://opportu.me/og-image.png"
+          />
         </Head>
         <body className="bg-gray-100">
           <Main />
